5eecfdb7efa790f0f9fd43cac9435e67ed50d8bf
Paul Gilbertson <Paul.Gilbertson@jncc.gov.uk>
2017-12-05 16:55:47 +0000

    Clean and Import data

---
+++ microservices/Controllers/BiotopeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace microservices.Controllers
+{
+    public class BiotopeController : Controller
+    {
+        // GET: Biotope
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
