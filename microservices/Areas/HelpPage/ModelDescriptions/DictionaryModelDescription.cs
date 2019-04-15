a3e092145be6afa035abf64d785d5c7f63141e8b
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2019-04-15 11:11:05 +0100

    Removing leftover umbraco dependencies from microservice

--- microservices/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace microservices.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- microservices/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace microservices.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
