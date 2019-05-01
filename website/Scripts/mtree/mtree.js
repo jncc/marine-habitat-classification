773660a9791ea40a669549d3b22b38681969176e
Cathy@JNCC <Cathy.Jin@jncc.gov.uk>
2019-05-01 16:35:56 +0100

    IE11 compatibility for tree

--- website/Scripts/mtree/mtree.js
+++ website/Scripts/mtree/mtree.js
@@ -1,8 +1,7 @@
 ﻿
 
-// mtree.js
-// Requires jquery.js and velocity.js (optional but recommended).
-// Copy the below function, add to your JS, and simply add a list <ul class=mtree> ... </ul>
+// Modified mtree.js
+// Requires jquery.js
 ; (function ($, window, document, undefined) {
     
     // Only apply if mtree list exists
@@ -10,11 +9,8 @@
 
         // Settings
         var start_level = 2; // Start with collapsed menu (only level 1 items visible)
-        var close_same_level = false; // Close elements on same level when opening new node.
         var duration = 400; // Animation duration should be tweaked according to easing.
-        var listAnim = false; // Animate separate list items on open/close element (velocity.js only).
-        var easing = 'easeOutQuart'; // Velocity.js only, defaults to 'swing' with jquery animation.
-
+        
 
         // Set initial styles 
         $('.mtree ul').css({ 'overflow': 'hidden', 'height': 0, 'display': 'none' });
@@ -50,88 +46,16 @@
             var el = $(this).parent().children('ul').first();
             var isOpen = $(this).parent().hasClass('mtree-open');
 
-            // close other elements on same level if opening 
-            if ((close_same_level || $('.csl').hasClass('active')) && !isOpen) {
-                var close_items = $(this).closest('ul').children('.mtree-open').not($(this).parent()).children('ul');
-
-                // Velocity.js
-                if ($.Velocity) {
-                    close_items.velocity({
-                        height: 0,
-                        duration: duration,
-                        easing: easing,
-                        display: 'none',
-                        delay: 100,
-                        complete: function () {
-                            setNodeClass($(this).parent(), true);
-                        }
-                    });
-
-                    // jQuery fallback
-                } else {
-                    close_items.delay(100).slideToggle(duration, function () {
-                        setNodeClass($(this).parent(), true);
-                    });
-                }
-            }
-
             // force auto height of element so actual height can be extracted
             el.css({ 'height': 'auto' });
 
-            // listAnim: animate child elements when opening
-            if (!isOpen && $.Velocity && listAnim) el.find(' > li, li.mtree-open > ul > li').css({ 'opacity': 0 }).velocity('stop').velocity('list');
-
-            // Velocity.js animate element
-            if ($.Velocity) {
-                el.velocity('stop').velocity({
-                    //translateZ: 0, // optional hardware-acceleration is automatic on mobile
-                    height: isOpen ? [0, el.outerHeight()] : [el.outerHeight(), 0],
-                    queue: false,
-                    duration: duration,
-                    easing: easing,
-                    display: isOpen ? 'none' : 'block'
-                }, {
-                    begin: setNodeClass($(this).parent(), isOpen),
-                    complete: function () {
-                        if (!isOpen) {
-                            $(this).css('height', 'auto');
-                        }
-                    }
-                });
-
-                // jQuery fallback animate element
-            } else {
-                setNodeClass($(this).parent(), isOpen);
-                el.slideToggle(duration);
-            }
+            setNodeClass($(this).parent(), isOpen);
+            el.slideToggle(duration);
+   
 
             // We can't have nodes as links unfortunately
             e.preventDefault();
         });
-
-        // List animation sequence
-        if ($.Velocity && listAnim) {
-            $.Velocity.Sequences.list = function (element, options, index, size) {
-                $.Velocity.animate(element, {
-                    opacity: [1, 0],
-                    translateY: [0, -(index + 1)]
-                }, {
-                        delay: index * (duration / size / 2),
-                        duration: duration,
-                        easing: easing
-                    });
-            };
-        }
-
-        // Fade in mtree after classes are added.
-        // Useful if you have set collapsed = true or applied styles that change the structure so the menu doesn't jump between states after the function executes.
-        if ($('.mtree').css('opacity') === 0) {
-            if ($.Velocity) {
-                $('.mtree').css('opacity', 1).children().css('opacity', 0).velocity('list');
-            } else {
-                $('.mtree').show(200);
-            }
-        }
     }
 
     // Function for updating node class
@@ -144,6 +68,5 @@
             el.children('a').first().children('span').first().removeClass('closed').addClass('open');
         }
     }
-
 
 }(jQuery, this, this.document));
